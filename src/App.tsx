5371d06238d569ab46722058b51edf3993c3566e
viraj-s15 <veer15102003@gmail.com>
2023-06-07 22:47:08 +0530

    Added routing and scaffolding for main comp

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import { Component, For } from "solid-js";
 import { hashIntegration, Route, Router, Routes, Link } from "solid-app-router";
 import RequestHandler from "./components/RequestHandler";
 import { restRequests } from "./store";
+import Hero from "./components/Hero";
 
 const App: Component = () => {
   return (
     <div class="flex items-center justify-start">
       <Router source={hashIntegration()}>
         <div class="max-w-[97%]">
-          <div class="h-[90vh] m-4 bg-gray-100 text-black rounded-md w-full flex flex-col gap-4 items-center">
+          <div class="h-[94vh] m-5 bg-gray-100 text-black rounded-xl w-full flex flex-col gap-4 items-center">
             <RequestHandler />
             <For each={restRequests()} fallback={<div>Loading...</div>}>
               {(item) => (
                 <Link href={`/${item.id}`}>
-                  <div class="border-2 border-black max-w-[18rem] p-4 text-xl bg-gray-100 hover:bg-gray-300 cursor-pointer rounded-lg mb-2">
+                  <div class="border-2 border-black max-w-[15rem] p-4 text-xl bg-gray-100 hover:bg-gray-300 cursor-pointer rounded-lg mb-2">
                     <div>{item.name}</div>
                     <div class="text-xs break-all">
                       {item.request.method} {item.request.url}
@@ -24,6 +25,7 @@ const App: Component = () => {
             </For>
           </div>
         </div>
+        <Hero />
       </Router>
     </div>
   );
---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import { Component } from "solid-js";
+
+const Hero: Component = () => {
+  return (
+    <div class="flex w-full m-6 rounded-xl text-center h-[94vh] text-black bg-gray-100 ">
+      This is the main Component and must appear throughout the rest of the
+      page. Its state will be managed soon
+    </div>
+  );
+};
+
+export default Hero;
